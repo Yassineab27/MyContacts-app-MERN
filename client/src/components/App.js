b368a58abb33daf6c0428f59f0bc4f7c919c4f8f
Yassineab27 <45517237+Yassineab27@users.noreply.github.com>
2019-07-01 12:05:45 +0100

    adding redux and fixing the navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ContactState from "../context/contact/ContactState";
 
-import Home from "./Home";
-import About from "./About";
-import Navbar from "./layouts/Navbar";
+import Contacts from "./Contacts";
+import Navbar from "./Navbar";
 import "../index.css";
 
 const App = () => {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </ContactState>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Contacts} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
